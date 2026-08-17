e155314da664471a4f3a8f6c1ba347bcdafc7010
ishantsil56-cell <ishantsil56@gmail.com>
2026-08-17 13:50:03 +0530

    Add API handler for menu items retrieval

---
+++ src/pages/api/menu.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { menuItems } from '@/data/menu';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    res.status(200).json(menuItems);
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
